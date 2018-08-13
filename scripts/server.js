69f50cc9bd05c427cdf989982ec5c431a096beef
jpacareu <jpacareu@nisum.com>
2018-08-13 14:34:38 -0300

    before-node-api

--- scripts/server.js
+++ scripts/server.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
+const request = require('superagent');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+const url = "http://api.openweathermap.org/data/2.5/";
+
+app.get('/weather/id', function (req, res) {
+    const {id, APPID} = req.query;
+    request.get(`${url}/weather`)
+    .query({id, APPID})
+    .then(resp => res.send(resp.text))
+    .catch(err => console.log(err.message))
 });
 
-app.get('/', function (req, res) {
- console.log("Hi")
- return res.send('Hello world');
+app.get('/weather/q', function (req, res) {
+    const {q, APPID} = req.query;
+    request.get(`${url}/weather`)
+    .query({q, APPID})
+    .then(resp => res.send(resp.text))
+    .catch(err => console.log(err.message))
 });
 
+app.get('/forecast/id', function (req, res) {
+    const {id, APPID} = req.query;
+    request.get(`${url}/forecast`)
+    .query({id, APPID})
+    .then(resp => res.send(resp.text))
+    .catch(err => console.log(err.message))
+});
+
+app.get('/forecast/q', function (req, res) {
+    const {q, APPID} = req.query;
+    request.get(`${url}/forecast`)
+    .query({q, APPID})
+    .then(resp => forecastHandler(res, resp.text))
+    .catch(err => console.log(err.message))
+});
+
+function forecastHandler(res,json){
+    res.send(json);
+}
 app.listen(process.env.PORT || 8081);
